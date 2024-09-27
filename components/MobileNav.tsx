d72120d69d07e0b5499871386f1b524e357da710
Hollow05 <kuanvell@gmail.com>
2024-09-27 13:07:36 +0700

    FEAT (DONE) sidebar link function done

---
+++ components/MobileNav.tsx
@@ -0,0 +1,17 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
+
+const MobileNav = () => {
+  return <section className="w-full max-w-[264px]">
+    MobileNav
+  </section>;
+};
+
+export default MobileNav;
