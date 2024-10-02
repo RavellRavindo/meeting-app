61edad1baacd79075bd034a677f37796600c590f
Hollow05 <kuanvell@gmail.com>
2024-10-02 16:41:48 +0700

    FEAT (DONE) toaster feature done

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,20 +4,66 @@ import React, { useState } from "react";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
 import MeetingModal from "./MeetingModal";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useUser } from "@clerk/nextjs";
+import { useToast } from "@/hooks/use-toast"
 
 const MeetingTypeList = () => {
-  
   const router = useRouter();
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    description: "",
+    link: "",
+  });
 
-  const createMeeting = () => {
-    console.log("Creating meeting...");
+  const [callDetails, setCallDetails] = useState<Call>();
+  const { toast } = useToast()
+
+  const createMeeting = async () => {
+    if (!client || !user) return;
+
+    try {
+      if (!values.dateTime) {
+        toast({ title: "please select a date and time" });
+        return;
+      }
+
+      const id = crypto.randomUUID();
+      const call = client.call("default", id);
+
+      if (!call) throw new Error("failed to create call");
+
+      const StartsAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || "Instant meeting";
+      await call.getOrCreate({
+        data: {
+          starts_at: StartsAt,
+          custom: {
+            description,
+          },
+        },
+      });
+
+      setCallDetails(call);
+      if (!values.description) {
+        router.push(`/meeting/${call.id}`);
+      }
+      toast({ title: "Meeting Created" });
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "failed to create meeting",
+      });
+    }
   };
-  
+
   console.log("Current meetingState:", meetingState);
-  
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
@@ -26,8 +72,8 @@ const MeetingTypeList = () => {
         tittle="New Meeting"
         description="Start an instant meeting"
         handleClick={() => {
-          console.log("clicked")
-          setMeetingState("isInstantMeeting")
+          console.log("clicked");
+          setMeetingState("isInstantMeeting");
         }}
         className="bg-orange-1"
       />
